ef190b662c074a0ec23887b3069e5c097edbb877
Fabio Luiz Vieira da Silva <fabio.luizvieira@globo.com>
2020-12-29 22:50:49 -0300

    Basic employee program using inheritance

---
+++ employee/src/entities/Employee.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class Employee {
+	
+	public String name;
+	public int hours;
+	public double valuePerHour;
+	
+	public Employee () {
+		
+	}
+
+	public Employee(String name, int hours, double valuePerHour) {		
+		this.name = name;
+		this.hours = hours;
+		this.valuePerHour = valuePerHour;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+	public double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+	
+	public double payment() {
+		return valuePerHour * hours;
+	}
+}
